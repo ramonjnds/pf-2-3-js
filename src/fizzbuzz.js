e89e65e93a7fb9608c14acb8f4cc20f6315ae566
ramonjnds <ramondejesusdominguezsolis@gmail.com>
2025-08-25 20:06:19 -0600

    Laboratorio 1 "FIZZBUZZ"

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,16 @@
 export function fizzbuzz(){
   //tu código va aqui
-  
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
+
+fizzbuzz(); // Ejecuta la función
